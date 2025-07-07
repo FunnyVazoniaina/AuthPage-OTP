a6370eb15d99336353de40d6f67c2f26b9dbc2bc
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-07-07 11:33:11 +0200

    Refactor[jwt, password]: move pwd and jwt management in separated files

---
+++ utils/jwt.js
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+}
+module.exports = generateToken;
