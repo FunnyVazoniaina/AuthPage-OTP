2eebd435cc61399e5a710aae34ab4ae38492d204
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-07-08 16:08:58 +0200

    Refactor: making code shorter by changing method export way

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const generateToken = (payload) => {
+exports.generateToken = (payload) => {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
 }
-module.exports = generateToken;
+
