3e97aaa8ac9b91b29b62a18412caab5ef6deeb8c
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-07-10 07:35:46 +0200

    CleanUp(password.js): removing comments its already stupid clear

--- utils/password.js
+++ utils/password.js
@@ -1,15 +1,12 @@
 const bcrypt = require('bcryptjs');
 
 class PasswordUtils {
-    // Hash a password
     static async hashPassword(password) {
         const salt = await bcrypt.genSalt(10);
         return await bcrypt.hash(password, salt);
     }
-    // Compare a password with a hashed password
-    static async comparePassword(password, hashedPassword) {
+        static async comparePassword(password, hashedPassword) {
         return await bcrypt.compare(password, hashedPassword);
     }
-
 }
 module.exports = PasswordUtils;
